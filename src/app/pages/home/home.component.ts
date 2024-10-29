e7f5075f012002375695814f5e66f7b5c2734813
alejandrop25 <alejandrop2506@gmail.com>
2024-10-28 21:01:03 -0300

    avanços

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { MainBarComponent } from "../../components/main-bar/main-bar.component";
 import { LeftMenuComponent } from "../../components/left-menu/left-menu.component";
+import { CentralImgComponent } from "../../components/central-img/central-img.component";
+import { RightMenuComponent } from "../../components/right-menu/right-menu.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MainBarComponent, LeftMenuComponent],
+  imports: [MainBarComponent, LeftMenuComponent, CentralImgComponent, RightMenuComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
